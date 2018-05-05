840b37b6cd16ea9906fe3b255fcd066e24a5527e
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-05 14:25:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,6 +9,6 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   return array
 }
 
-function addElementToEndOfArray (array, element) {
+function addElementToEndOfArray(array, element) {
   return [array, element]
 }
